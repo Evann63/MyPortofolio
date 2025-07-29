59e40075db9bf143c8429fe1729f0924f4a73ae2
Evann63 <evanderjuventino@gmail.com>
2025-07-29 13:25:09 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById("toggleMode").addEventListener("click", function() {
+    document.body.classList.toggle("dark-mode");
+    
+    let btn = document.getElementById("toggleMode");
+    if (document.body.classList.contains("dark-mode")) {
+        btn.textContent = "☀️ Light Mode";
+    } else {
+        btn.textContent = "🌙 Dark Mode";
+    }
+});
